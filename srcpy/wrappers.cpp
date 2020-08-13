71514645e3726239f5e7954fa2f0866a291f9c73
Vincent Berenz <vberenz@tuebingen.mpg.de>
2020-08-13 16:48:00 +0200

    updated pybind wrappers to use templated config

--- srcpy/wrappers.cpp
+++ srcpy/wrappers.cpp
@@ -3,9 +3,10 @@
 
 PYBIND11_MODULE(o80_example, m)
 {
-    o80::create_python_bindings<o80_example::Driver,
-                                o80_example::Standalone,
-                                double,
-                                double>  // int,int are arguments to drivers
-        (m);
+  o80::create_python_bindings<o80_example::Standalone>(m);
+  o80::create_standalone_python_bindings<o80_example::Driver,
+					 o80_example::Standalone,
+					 double,
+					 double>  // double,double are arguments to drivers
+    (m);
 }
